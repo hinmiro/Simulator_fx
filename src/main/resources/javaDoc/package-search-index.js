05ef0e7be647630371f2a8d54ee00dafd2ee0d57
mirohi <m_hintikka@hotmail.com>
2024-03-18 16:25:11 +0200

    javadoc

---
+++ src/main/resources/javaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"dao"},{"l":"datasource"},{"l":"entity"},{"l":"simu.model"},{"l":"view"}];updateSearchResults();
